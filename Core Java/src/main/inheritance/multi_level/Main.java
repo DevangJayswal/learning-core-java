c1d9fbd0b07fd60053b39bc390d7209c3eda9cf3
st-201906141 <st-201906141@L2-R2-L007.mcit.local>
2019-07-26 20:26:54 -0400

    SOMETHING

--- Core Java/src/main/inheritance/multi_level/Main.java
+++ Core Java/src/main/inheritance/multi_level/Main.java
@@ -1,27 +1,27 @@
 package main.inheritance.multi_level;
 
 public class Main extends C implements A, B {
-	
-	int a=1;
-	
+
+	int a = 1;
+
 	public void hello() {
 		System.out.println("Main.hello");
 	}
-	
+
 	public static void main(String[] args) {
-		
-		
+
 		A a = new Main();
-		B b = new Main();
-		C c = new Main();
+		B b = new Main();		
 		C cm = new Main();
-		
-		
-		b.hello();
-		System.out.println("A.a=" + a.a);
-		System.out.println("B.a=" + b.a);
-		System.out.println("C.a=" + c.a);
-		cm.hello();
-		System.out.println("CM.a=" + cm.a);		
+		C c = new C();
+
+		System.out.println("A.a=" + a.a);//2 
+		a.hello();//Main.hello
+		System.out.println("B.a=" + b.a);//3
+		b.hello();//Main.hello
+		System.out.println("CM.a=" + cm.a);//4
+		cm.hello();//Main.hello
+		System.out.println("C.a=" + c.a);//4
+		c.hello();//C.hello
 	}
 }
